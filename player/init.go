ff436687b7410ae1eb4d726292c3e6254f468b38
154pinkchairs <ovehis@riseup.net>
2022-09-09 03:25:58 +0200

    further division

--- player/init.go
+++ player/init.go
@@ -6,3 +6,23 @@ type Player struct {
 	Reputation, WantedLevel int
 	cash, debt              int
 }
+
+func NewPlayer(name string) *Player {
+	return &Player{
+		Name:        name,
+		Health:      100,
+		Reputation:  0,
+		WantedLevel: 0,
+		cash:        5000,
+		debt:        10000,
+	}
+}
+
+func ModPlayer(reputation int, wantedLevel int, cash int, debt int) *Player {
+	return &Player{
+		Reputation:  reputation,
+		WantedLevel: wantedLevel,
+		cash:        cash,
+		debt:        debt,
+	}
+}
