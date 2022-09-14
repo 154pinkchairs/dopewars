058d644084172d76ed14cb8369a3699b766dfc1c
154pinkchairs <ovehis@riseup.net>
2022-09-14 23:48:05 +0200

    add a main menu window iwth a background image

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("assets/menu_bg.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Game struct{}
 
@@ -14,11 +25,11 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Welcome to the city of New York.")
+	screen.DrawImage(img, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 854, 480
+	return 960, 540
 }
 
 func main() {
@@ -28,7 +39,8 @@ func main() {
 		log.Fatal(err)
 	}
 }
-func execute() {
+
+/*func execute() {
 	//create a character
 	//create a city
 	//create a district
@@ -36,3 +48,4 @@ func execute() {
 	main()
 	//basegame.Player.Character()
 }
+*/
