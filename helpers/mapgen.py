6c528840308cacc04d77b0d0514577765da825ce
154pinkchairs <mholub@tutanota.com>
2022-12-11 05:06:08 +0100

    old street placement system

--- helpers/mapgen.py
+++ helpers/mapgen.py
@@ -50,65 +50,125 @@ tile_symbols = {
     Tile.metro: 'm',
 }
 
+# keeping that for later in case imperative mapgen fails
+#width = int(argv[1])
+#length = int(argv[2])
+#filename = argv[3]
+class Borough:
+    def __init__(self, name, width, length):
+        self.name = name
+        self.width = width
+        self.length = length
+        #self.map = np.zeros((self.length, self.width), dtype=np.int8)
+        #self.map.fill(Tile.grass)
 
-width = int(argv[1])
-height = int(argv[2])
-filename = argv[3]
+bronx = Borough('bronx', 300, 300)
+queens = Borough('queens', int(bronx.width * 2.3), int(bronx.height * 2.3))
+manhattan = Borough('manhattan', int(bronx.width * 1.5), int(bronx.height * 1.9))
+staten_island = Borough('staten_island', int(bronx.width * 1.2), int(bronx.height * 1.2))
+brooklyn = Borough('brooklyn', int(bronx.width * 1.8), int(bronx.height * 1.8))
 
-def write_map(width, height, filename):
+boroughs = [bronx, queens, manhattan, staten_island, brooklyn]
+
+for borough in boroughs:
+    map_array = np.random.randint(1,19, size=(borough.length, borugh.width))
+
+def write_map(width, length, filename):
   # create a directory for the map files if it does not exist
   if not path.exists('../maps'):
     makedirs('../maps')
 
-  # create a numpy array of the given dimensions
-  map_array = np.zeros((height, width), dtype=np.int8)
   # convert the integer values in the map_array to their corresponding symbols
   symbol_map_array = np.vectorize(tile_symbols.get)(map_array)
+    # place the streets and alleys on the map
+  x = random.randint(0, borough.width - 1)
+  y = random.randint(0, borough.length - 1)
+  map_array[y][x] = random.choice([Tile.horiz_st, Tile.vert_st, Tile.alley_horiz, Tile.alley_vert])
+    #keep moving in a random direction until we hit the edge of the map
+  while x > 0 and x < borough.width - 1 and y > 0 and y < borough.length - 1:
+        dx, dy = random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
+        x += dx
+        y += dy
+        map_array[y][x] = random.choice([Tile.horiz_st, Tile.vert_st, Tile.alley_horiz, Tile.alley_vert])
+        #move in the selected random direction
+        if random.randint(0, 1) == 0:
+            #move horizontally
+            x += direction
+            #ensure that buildings form frontages no longer than 5 tiles
+            if x > 0 and x < width-1 and map_array[y][x-1] == map_array[y][x+1]:
+                #if the previous and next tiles are the same, then we are in the middle of a frontage
+                map_array[y][x] = random.choice([Tile.horiz_st, Tile.alley_horiz])
+            else:
+                #otherwise, we are at the end of a frontage
+                map_array[y][x] = random.choice([Tile.horiz_st, Tile.alley_horiz, Tile.building_a, Tile.building_b, Tile.building_c])
+        else:
+            #move vertically
+            y += direction
+            if y > 0 and y < length-1 and map_array[y-1][x] == map_array[y+1][x]:
+                map_array[y][x] = random.choice([Tile.vert_st, Tile.alley_vert])
+            else:
+                map_array[y][x] = random.choice([Tile.vert_st, Tile.alley_vert, Tile.building_a, Tile.building_b, Tile.building_c])
+    #place the trees on the map. Trees must be surrounded by grass. In Manhattan, we'll create Central Park
+  for i in range(0, width):
+        for j in range(0, length):
+            if map_array[i][j] == Tile.grass:
+                if i < 0 or i >= len(map_array):
+                    continue
+                if j < 0 or j >= len(map_array[i]):
+                    continue
+                if random.randint(0, 100) < 5:
+                    map_array[i][j] = Tile.tree
+
+
+
   # calculate the number of tiles for each feature
-  num_banks = int(4 * (width * height / (300 * 300)))
-  num_hospitals = int(1 * (width * height / (300 * 300)))
-  num_police_stations = int(1 * (width * height / (300 * 300)))
-  num_traphouses = int(3 * (width * height / (300 * 300)))
-  num_gunshops = int(2 * (width * height / (300 * 300)))
-  num_metro_stations = int(1 * (width * height / (300 * 300)))
-  num_parks = int(0.15 * width * height)
-  num_water = int(0.1 * width * height)
+  num_banks = int(4 * (width * length / (300 * 300)))
+  num_hospitals = int(1 * (width * length / (300 * 300)))
+  num_police_stations = int(1 * (width * length / (300 * 300)))
+  num_traphouses = int(3 * (width * length / (300 * 300)))
+  num_gunshops = int(2 * (width * length / (300 * 300)))
+  num_metro_stations = int(1 * (width * length / (300 * 300)))
+  num_parks = int(0.15 * width * length)
+  num_water = int(0.1 * width * length)
 
   # place the features on the map
   for i in range(num_banks):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.bank
   for i in range(num_hospitals):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.hosp
   for i in range(num_police_stations):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.police_st
   for i in range(num_traphouses):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.traphouse
   for i in range(num_gunshops):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.gunshop
   for i in range(num_metro_stations):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.metro
   for i in range(num_parks):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.grass
   for i in range(num_water):
     x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
+    y = random.randint(0, length-1)
     symbol_map_array[y][x] = Tile.water
   # write the map array to a file
   np.savetxt('../maps/' + filename + '.map', symbol_map_array, fmt='%s', delimiter='')
 
-if __name__ == '__main__':
-    write_map(width, height, filename)
+write_map(bronx_width, bronx_length, 'bronx')
+write_map(manhattan_width, manhattan_length, 'manhattan')
+write_map(staten_island_width, staten_island_length, 'staten_island')
+write_map(brooklyn_width, brooklyn_length, 'brooklyn')
+write_map(queens_width, queens_length, 'queens')
--- main.go
+++ main.go
@@ -1,149 +1,133 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"image/color"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-	"sync"
-
-	"github.com/154pinkchairs/dopewars2d/helpers"
+	"os/exec"
+	"runtime"
 
 	"github.com/154pinkchairs/dopewars2d/basegame"
 	"github.com/154pinkchairs/dopewars2d/core"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/tinne26/etxt"
 	"github.com/yohamta/furex/v2"
 	"github.com/yohamta/furex/v2/components"
 	"golang.org/x/exp/shiny/screen"
 )
 
 type Game struct {
- init         bool
-	gameUI    *furex.View
-	screen    screen.Screen
-	Character basegame.Character
-	CG        core.Game
+	init        bool
+	gameUI      *furex.View
+	screen      screen.Screen
+	Character   basegame.Character
+	txtRenderer *etxt.Renderer
+	CG          core.Game
 	//must implement ebiten.Game interface
 	ebiten.Game
 }
 
 var bg *ebiten.Image
 
-var (
-	Bg                *ebiten.Image
-	Loadsave          *ebiten.Image
-	Newgameimg        *ebiten.Image
-	Donate            *ebiten.Image
-	Issues            *ebiten.Image
-	Quitimg           *ebiten.Image
-	Loadsave_hoover   *ebiten.Image
-	Newgameimg_hoover *ebiten.Image
-	Donate_hoover     *ebiten.Image
-	Issues_hoover     *ebiten.Image
-	Quitimg_hoover    *ebiten.Image
-	InitDone          chan bool
-	wg                sync.WaitGroup
-)
+var loadsave *ebiten.Image
+var newgameimg *ebiten.Image
+var donate *ebiten.Image
+var issues *ebiten.Image
+var quitimg *ebiten.Image
+var loadsave_hoover *ebiten.Image
+var newgameimg_hoover *ebiten.Image
+var donate_hoover *ebiten.Image
+var issues_hoover *ebiten.Image
+var quitimg_hoover *ebiten.Image
 
-func Init() error {
+func init() {
 	var err error
-	wg.Add(1)
-	Bg, _, _ = ebitenutil.NewImageFromFile("assets/menu_bg.png")
-	Newgameimg, _, _ = ebitenutil.NewImageFromFile("assets/newgame.png")
-	Loadsave, _, _ = ebitenutil.NewImageFromFile("assets/loadsave.png")
-	Donate, _, _ = ebitenutil.NewImageFromFile("assets/donate.png")
-	Issues, _, _ = ebitenutil.NewImageFromFile("assets/issues.png")
-	Quitimg, _, _ = ebitenutil.NewImageFromFile("assets/quit.png")
-	Loadsave_hoover, _, _ = ebitenutil.NewImageFromFile("assets/loadsave_hoover.png")
-	Newgameimg_hoover, _, _ = ebitenutil.NewImageFromFile("assets/newgame_hoover.png")
-	Donate_hoover, _, _ = ebitenutil.NewImageFromFile("assets/donate_hoover.png")
-	Issues_hoover, _, _ = ebitenutil.NewImageFromFile("assets/issues_hoover.png")
-	Quitimg_hoover, _, err = ebitenutil.NewImageFromFile("assets/quit_hoover.png")
+	bg, _, _ = ebitenutil.NewImageFromFile("assets/menu_bg.png")
+	newgameimg, _, _ = ebitenutil.NewImageFromFile("assets/newgame.png")
+	loadsave, _, _ = ebitenutil.NewImageFromFile("assets/loadsave.png")
+	donate, _, _ = ebitenutil.NewImageFromFile("assets/donate.png")
+	issues, _, _ = ebitenutil.NewImageFromFile("assets/issues.png")
+	quitimg, _, _ = ebitenutil.NewImageFromFile("assets/quit.png")
+	loadsave_hoover, _, _ = ebitenutil.NewImageFromFile("assets/loadsave_hoover.png")
+	newgameimg_hoover, _, _ = ebitenutil.NewImageFromFile("assets/newgame_hoover.png")
+	donate_hoover, _, _ = ebitenutil.NewImageFromFile("assets/donate_hoover.png")
+	issues_hoover, _, _ = ebitenutil.NewImageFromFile("assets/issues_hoover.png")
+	quitimg_hoover, _, err = ebitenutil.NewImageFromFile("assets/quit_hoover.png")
 	if err != nil {
 		log.Fatal(err)
 	}
-	InitDone <- true
-	wg.Wait()
-	wg.Done()
-	return nil
 }
 
-func ClearScreen() error {
-	Bg = nil
-	Loadsave = nil
-	Newgameimg = nil
-	Donate = nil
-	Issues = nil
-	Quitimg = nil
-	Loadsave_hoover = nil
-	Newgameimg_hoover = nil
-	Donate_hoover = nil
-	Issues_hoover = nil
-	Quitimg_hoover = nil
+func (g *Game) Update() error {
+	if !g.init {
+		g.init = true
+		g.setupUI()
+	}
+	g.gameUI.UpdateWithSize(ebiten.WindowSize())
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go Init()
-	wg.Wait()
-	wg.Done()
-
-	<-InitDone
-
-	wg.Add(1)
-	go g.setupUI()
-	go g.gameUI.Draw(screen)
-	wg.Wait()
-	wg.Done()
-
-	screen.DrawImage(Bg, nil)
+	screen.DrawImage(bg, nil)
+	g.gameUI.Draw(screen)
 	pos1 := &ebiten.DrawImageOptions{}
 	pos1.GeoM.Translate(340, 150)
-	screen.DrawImage(Newgameimg, pos1)
+	screen.DrawImage(newgameimg, pos1)
 	if mouseOverButton(340, 150, 200, 50) {
-		screen.DrawImage(Newgameimg_hoover, pos1)
+		screen.DrawImage(newgameimg_hoover, pos1)
 	}
 	// add a handler for the new game button using MouseleftButtonHandler from furex
 
 	pos2 := &ebiten.DrawImageOptions{}
 	pos2.GeoM.Translate(340, 200)
-	screen.DrawImage(Loadsave, pos2)
+	screen.DrawImage(loadsave, pos2)
 	if mouseOverButton(340, 200, 200, 50) {
-		screen.DrawImage(Loadsave_hoover, pos2)
+		screen.DrawImage(loadsave_hoover, pos2)
 	}
 
 	pos3 := &ebiten.DrawImageOptions{}
 	pos3.GeoM.Translate(340, 250)
-	screen.DrawImage(Donate, pos3)
+	screen.DrawImage(donate, pos3)
 	if mouseOverButton(340, 250, 200, 50) {
-		screen.DrawImage(Donate_hoover, pos3)
+		screen.DrawImage(donate_hoover, pos3)
 	}
 
 	pos4 := &ebiten.DrawImageOptions{}
 	pos4.GeoM.Translate(340, 300)
-	screen.DrawImage(Issues, pos4)
+	screen.DrawImage(issues, pos4)
 	if mouseOverButton(340, 300, 200, 50) {
-		screen.DrawImage(Issues_hoover, pos4)
+		screen.DrawImage(issues_hoover, pos4)
 	}
 
 	pos5 := &ebiten.DrawImageOptions{}
 	pos5.GeoM.Translate(340, 350)
-	screen.DrawImage(Quitimg, pos5)
+	screen.DrawImage(quitimg, pos5)
 	if mouseOverButton(340, 350, 280, 50) {
-		screen.DrawImage(Quitimg_hoover, pos5)
+		screen.DrawImage(quitimg_hoover, pos5)
 	}
+}
+
+func clearScreen() error {
+	bg.Clear()
+	newgameimg.Clear()
+	loadsave.Clear()
+	donate.Clear()
+	issues.Clear()
+	quitimg.Clear()
+	loadsave_hoover.Clear()
+	donate_hoover.Clear()
+	issues_hoover.Clear()
+	quitimg_hoover.Clear()
+	return nil
 }
 
 // TODO: convert the functions called here to gorooutines
 func (g *Game) StartGame(c *basegame.Character, cg *core.Game) error {
 	//run the game
-	ClearScreen()
+	clearScreen()
 	c.InitDefault()
 	core.NewGame(c, cg)
 	g.CG.HasStarted = true
@@ -171,8 +155,8 @@ func (g *Game) setupUI() {
 			Position:     0,
 			Handler: &components.Button{Text: "", OnClick: func() {
 				g.Update()
-
-				g.StartGame(&g.Character, &g.CG)
+				cg := core.Game{}
+				g.StartGame(&g.Character, &cg)
 			},
 			},
 			Direction:    0,
@@ -199,7 +183,12 @@ func (g *Game) setupUI() {
 				basegame.Loadsave(&basegame.Character{})
 				//if savegame.json file does not exist, create it
 				basegame.NewGame(&basegame.Game{})
-				ClearScreen()
+				bg.Clear()
+				newgameimg.Clear()
+				loadsave.Clear()
+				donate.Clear()
+				issues.Clear()
+				quitimg.Clear()
 			},
 			},
 		})
@@ -215,7 +204,7 @@ func (g *Game) setupUI() {
 			MarginTop:    5,
 			MarginRight:  5,
 			MarginBottom: 5,
-			Handler:      &components.Button{Text: "", OnClick: func() { helpers.Openbrowser("https://www.liberapay.com/") }}, //TODO: setup donations
+			Handler:      &components.Button{Text: "", OnClick: func() { openbrowser("https://www.liberapay.com/") }}, //TODO: setup donations
 		})
 	}
 
@@ -229,7 +218,7 @@ func (g *Game) setupUI() {
 			MarginTop:    5,
 			MarginRight:  5,
 			MarginBottom: 5,
-			Handler:      &components.Button{Text: "", OnClick: func() { helpers.Openbrowser("https://github.com/154pinkchairs/dopewars/issues") }},
+			Handler:      &components.Button{Text: "", OnClick: func() { openbrowser("https://github.com/154pinkchairs/dopewars/issues") }},
 			Wrap:         furex.NoWrap,
 		})
 	}
@@ -301,6 +290,8 @@ func (g *Game) setupUI() {
 		issuesBtn(),
 		quitBtn(),
 	)
+	//if core.NewGame function has started, then .RemoveAll is called on the gameUI
+	//NOTE: got segfault here
 	if g.CG.HasStarted {
 		g.gameUI.RemoveAll()
 	}
@@ -319,18 +310,23 @@ func mouseOverButton(x, y, width, height int) bool {
 	return false
 }
 
-func resflag() (int, int) {
-	//get the resolution in intxint format, parsing the string and converting it to 2 ints
-	res := strings.Split(flag.Arg(0), "x")
-	resx, err := strconv.Atoi(res[0])
-	if err != nil {
-		panic(err)
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
 	}
-	resy, err := strconv.Atoi(res[1])
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	return resx, resy
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -341,21 +337,7 @@ func main() {
 	ebiten.SetWindowSize(960, 540)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.SetWindowTitle("Dopewars 2D")
-	help := flag.Bool("help", false, "show help")
-	flag.Parse()
-	if *help {
-		log.Println("Usage: dopewars [options] [resolution]")
-		log.Println("Options:")
-		flag.PrintDefaults()
-		return
-	}
-	debug := flag.Bool("debug", false, "debug mode")
-	flag.Parse()
-	if *debug {
-		log.Println("Debug mode enabled")
-		log.SetFlags(log.LstdFlags | log.Lshortfile)
-		log.SetPrefix("DEBUG: ")
-	} else if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
